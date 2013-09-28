0915bb4a00462b98e28e3f2cd750484a547d32de
Dmitry Kondrashkin <kondra2lp@gmail.com>
2013-09-28 20:53:58 +0400

    refactor weak-supervision experiments

--- lssvm_test.py
+++ lssvm_test.py
@@ -1,11 +1,12 @@
 import numpy as np
-
-from latent_crf import LatentCRF
-from edge_crf import EdgeCRF
-from latent_structured_svm import LatentSSVM
-from heterogenous_crf import HCRF
+import cPickle
 
 from pystruct.learners import OneSlackSSVM
+from time import time
+
+from latent_crf import LatentCRF
+from latent_structured_svm import LatentSSVM
+from heterogenous_crf import HCRF
 
 from data_loader import load_syntetic
 from common import compute_error
@@ -14,7 +15,9 @@ from common import weak_from_hidden
 # testing with weakly labeled train set
 
 
-def test_syntetic(mode):
+def test_syntetic_weak(mode):
+    # Syntetic data
+    # test latentSSVM on different train set sizes & on different train sets
     # mode can be 'heterogenous' or 'latent'
     results = np.zeros((18, 6))
     full_labeled = np.array([0, 2, 4, 10, 25, 100])
@@ -22,6 +25,7 @@ def test_syntetic(mode):
 
     for dataset in xrange(1, 19):
         X, H = load_syntetic(dataset)
+        H = list(H)
         Y = weak_from_hidden(H)
 
         for j, nfull in enumerate(full_labeled):
@@ -72,5 +76,44 @@ def test_syntetic(mode):
     return results
 
 
+def syntetic_weak():
+    models_basedir = 'models/syntetic/'
+    crf = HCRF(n_states=10, n_features=10, n_edge_features=2,
+               inference_method='gco')
+    base_clf = OneSlackSSVM(crf, max_iter=500, C=0.1, verbose=2,
+                            tol=0.001, n_jobs=4, inference_cache=100)
+    clf = LatentSSVM(base_clf, latent_iter=5, verbose=2)
+
+    X, H = load_syntetic(1)
+    X = list(X)
+    H = list(H)
+    Y = weak_from_hidden(H)
+
+    x_train = X[:400]
+    y_train = Y[:400]
+    h_train = H[:400]
+    x_test = X[401:]
+    y_test = Y[401:]
+    h_test = H[401:]
+
+    for i in xrange(10, len(h_train)):
+        h_train[i] = None
+
+    start = time()
+    clf.fit(x_train, y_train, h_train, pass_labels=True, initialize=True)
+    stop = time()
+
+    np.savetxt(models_basedir + 'syntetic_weak.csv', clf.w)
+    with open(models_basedir + 'syntetic_weak' + '.pickle', 'w') as f:
+        cPickle.dump(clf, f)
+
+    h_pred = clf.predict_latent(x_test)
+
+    print 'Error on test set: %f' % compute_error(h_test, h_pred)
+    print 'Norm of weight vector: |w|=%f' % np.linalg.norm(clf.w)
+    print 'Elapsed time: %f s' % (stop - start)
+
+
 if __name__ == '__main__':
-    test_syntetic('heterogenous')
+    syntetic_weak()
+#    test_syntetic_weak('heterogenous')
