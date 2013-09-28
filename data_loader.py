2e8ad394cc150d0436de536bb104f46cc61136e2
Dmitry Kondrashkin <kondra2lp@gmail.com>
2013-09-28 23:21:01 +0400

    add superpixel areas in syntetic dataset

--- data_loader.py
+++ data_loader.py
@@ -10,17 +10,19 @@ def load_msrc(dataset):
         npz = np.load(filename)
         return zip(npz['Xunary_train'],
                    npz['edges_train'],
-                   npz['Xpair_train']), npz['Y_train']
+                   npz['Xpair_train']), list(npz['Y_train'])
     if dataset == 'test':
         filename = base + 'msrc/test_data.npz'
         npz = np.load(filename)
         return zip(npz['Xunary_test'],
                    npz['edges_test'],
-                   npz['Xpair_test']), npz['Y_test']
+                   npz['Xpair_test']), list(npz['Y_test'])
 
 
 def load_syntetic(dataset):
     # fast loader
     filename = base + 'syntetic/features%d.npz' % dataset
     npz = np.load(filename)
-    return npz['X'], npz['Y']
+    # add superpixel area, which is one because pixel == superpixel in this dataset
+    Y = [np.vstack([y, np.ones(y.size)]).T for y in list(npz['Y'])]
+    return list(npz['X']), Y
