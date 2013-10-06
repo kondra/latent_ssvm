02a221473baa2984dde50a8066a4552e41a02861
Dmitry Kondrashkin <kondra2lp@gmail.com>
2013-10-06 15:23:26 +0400

    add __eq__ operator

--- label.py
+++ label.py
@@ -10,6 +10,9 @@ class Label(object):
         if self.full is None and self.weak is None:
             raise ValueError("You should specify full or weak labels")
         if self.weak is None:
-            self.weak = np.unique(self.full)
+            self.weak = np.unique(self.full).astype(np.int32)
         if self.full is None:
-            self.full = np.random.choice(self.weak, self.weights.size)
+            self.full = np.random.choice(self.weak, self.weights.size).astype(np.int32)
+
+    def __eq__(self, other):
+        return np.all(self.full == other.full)
