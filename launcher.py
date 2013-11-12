fe7441c74c523dcf976ae2a7306369072edf4599
Dmitry Kondrashkin <kondra2lp@gmail.com>
2013-11-12 11:06:29 +0400

    msrc experiment

--- launcher.py
+++ launcher.py
@@ -1,5 +1,6 @@
 from test_weak_labeled import msrc_weak
 
-result = msrc_weak('msrc lower regularization, alpha=1', max_iter=10000, latent_iter=3, inner_tol=0.1,
+result = msrc_weak('msrc medium regularization, alpha=1',
+                   max_iter=10000, latent_iter=3, inner_tol=0.1,
                    min_changes=20, outer_tol=0.1,
-                   initialize=True, C=1000, alpha=1, n_inference_iter=5)
+                   initialize=True, C=100, alpha=1, n_inference_iter=5)
