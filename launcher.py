b9f62922e63a65f6463882166bf7c380731ec899
Dmitry Kondrashkin <kondra2lp@gmail.com>
2013-12-02 23:19:38 +0400

    remove launcher script from git

--- launcher.py
+++
@@ -1,6 +0,0 @@
-from test_weak_labeled import msrc_weak
-
-result = msrc_weak('msrc medium regularization, alpha=1',
-                   max_iter=10000, latent_iter=3, inner_tol=0.1,
-                   min_changes=20, outer_tol=0.1,
-                   initialize=True, C=100, alpha=1, n_inference_iter=5)
