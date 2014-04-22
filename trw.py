f82ed7f32ad1e36b98d674d919ee5cb45069ded0
Dmitry Kondrashkin <kondra2lp@gmail.com>
2014-04-23 00:49:32 +0400

    rewrite trw

--- trw.py
+++ trw.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+from graph_utils import decompose_graph, decompose_grid_graph
 
-def optimize_chain(chain, unary_cost, pairwise_cost,
-                   return_energy=True):
-    n_states = unary_cost.shape[1]
+
+def optimize_chain(chain, unary_cost, pairwise_cost, edge_index):
     n_nodes = chain.shape[0]
+    n_states = unary_cost.shape[1]
 
     p = np.zeros((n_states, n_nodes))
     track = np.zeros((n_states, n_nodes), dtype=np.int32)
@@ -13,18 +14,18 @@ def optimize_chain(chain, unary_cost, pairwise_cost,
 
     for i in xrange(1, n_nodes):
         p[:,i] = unary_cost[i,:]
-        p_cost = pairwise_cost[(chain[i - 1], chain[i])]
+        p_cost = pairwise_cost[edge_index[(chain[i - 1], chain[i])]]
         for k in xrange(n_states):
-            p[k,i] += np.min(p[:,i - 1] + p_cost[:,k])
-            track[k,i] = np.argmin(p[:,i - 1] + p_cost[:,k])
+            p[k,i] += np.max(p[:,i - 1] + p_cost[:,k])
+            track[k,i] = np.argmax(p[:,i - 1] + p_cost[:,k])
 
     x = np.zeros(n_nodes, dtype=np.int32)
-    current = np.argmin(p[:,n_nodes - 1])
+    current = np.argmax(p[:,n_nodes - 1])
     for i in xrange(n_nodes - 1, -1, -1):
         x[i] = current
         current = track[current,i]
 
-    return x, np.min(p[:,n_nodes - 1])
+    return x, np.max(p[:,n_nodes - 1])
 
 
 def get_labelling(relaxed_x):
@@ -32,98 +33,94 @@ def get_labelling(relaxed_x):
     x = np.zeros(n_nodes)
     for i in xrange(n_nodes):
         x[i] = np.where(relaxed_x[i,:])[0][0]
-    return x
+    return x.astype(np.int32)
 
 
 def trw(node_weights, edges, edge_weights,
-        max_iter=100, relaxed=False, verbose=0,
-        smart_step=False, return_energy_history=False):
+        max_iter=100, verbose=0, tol=1e-3):
+
+    result = decompose_grid_graph([(node_weights, edges, edge_weights)])
+    contains_node, chains, edge_index = result[0][0], result[1][0], result[2][0]
+
     n_nodes, n_states = node_weights.shape
-    n_edges = edges.shape[0]
 
-    pairwise_cost = {}
-    for i in xrange(n_edges):
-        pairwise_cost[(edges[i,0], edges[i,1])] = edge_weights[i,:]
-
-    x_opt = []
-    chains = []
-    unaries = []
+    y_hat = []
     lambdas = []
-    trees = {k : [] for k in xrange(n_nodes)}
-    decode = {}
-    for i in xrange(1, 400, 20):
-        chains.append(np.arange(i, i + 20) - 1)
-        x_opt.append(np.zeros(20))
-        lambdas.append(np.zeros((20, n_states)))
-        unaries.append(node_weights[chains[-1],:] * 0.5)
-        tree_number = len(chains) - 1
-        for j, k in enumerate(chains[-1]):
-            decode[(k,tree_number)] = j
-            trees[k].append(tree_number)
-    for i in xrange(1, 21):
-        chains.append(np.arange(i, 401, 20) - 1)
-        x_opt.append(np.zeros(20))
-        lambdas.append(np.zeros((20, n_states)))
-        unaries.append(node_weights[chains[-1],:] * 0.5)
-        tree_number = len(chains) - 1
-        for j, k in enumerate(chains[-1]):
-            decode[(k,tree_number)] = j
-            trees[k].append(tree_number)
+    multiplier = []
 
+    for p in xrange(n_nodes):
+        multiplier.append(1.0 / len(contains_node[p]))
+    for chain in chains:
+        lambdas.append(np.zeros((len(chain), n_states)))
+        y_hat.append(np.zeros(len(chain)))
+
+    multiplier = np.array(multiplier)
+    multiplier.shape = (n_nodes, 1)
+
+    learning_rate = 0.1
     energy_history = []
-    alpha = 1.
-    gamma = 1.
-    delta = 1.
-    rho0 = 1.5
-    rho1 = 0.1
-    eps = 0.001
-    best_dual = -np.inf
+
+    adaptive_rate = False
+
+#    alpha = 1.
+#    gamma = 1.
+#    delta = 1.
+#    rho0 = 1.5
+#    rho1 = 0.1
+#    eps = 0.001
+#    best_dual = -np.inf
     for iteration in xrange(max_iter):
-        energy = 0.
+        energy = 0.0
+        unaries = node_weights * multiplier
+
         for i, chain in enumerate(chains):
-            x_opt[i], e = optimize_chain(chain, unaries[i]+lambdas[i], pairwise_cost)
+            y_hat[i], e = optimize_chain(chain,
+                                         lambdas[i] + unaries[chain,:],
+                                         edge_weights,
+                                         edge_index)
+
             energy += e
-        
-        mean_x = np.zeros((n_nodes, n_states), dtype=np.float64)
-        for i in xrange(n_nodes):
-            for k in trees[i]:
-                mean_x[i][x_opt[k][decode[(i,k)]]] += 1
-        mean_x *= 0.5
 
-        for i, chain in enumerate(chains):
-            x = np.zeros((20, n_states), dtype=np.float64)
-            x[np.arange(20), x_opt[i]] = 1
-            lambdas[i] += alpha * (x - mean_x[chain,:])
+        lambda_sum = np.zeros((n_nodes, n_states), dtype=np.float64)
+        for p in xrange(n_nodes):
+            assert len(contains_node[p]) == 2
+            for i in contains_node[p]:
+                pos = np.where(chains[i] == p)[0][0]
+                lambda_sum[p, y_hat[i][pos]] += multiplier[p]
 
-        if verbose > 2:
-            print 'iteration {}: energy = {}'.format(iteration, energy)
+        for i in xrange(len(chains)):
+            N = lambdas[i].shape[0]
 
-        if energy > best_dual:
-            best_dual = energy
+            lambdas[i][np.ogrid[:N], y_hat[i]] -= learning_rate
+            lambdas[i] += learning_rate * lambda_sum[chains[i],:]
+
         if iteration:
-            if smart_step:
-                grad_norm = np.sum((mean_x * 2) ** 2)
-                if energy > prev_energy:
-                    delta *= rho0
-                else:
-                    delta = max(rho1 * delta, eps)
-                alpha = (best_dual + delta) - energy
-                alpha /= grad_norm
-                alpha *= gamma
-            else:
-                alpha = 2 / np.sqrt(iteration)
+            learning_rate = 1. / np.sqrt(iteration)
 
-        prev_energy = energy
         energy_history.append(energy)
 
-    if verbose > 0:
-        print 'number of unconsistent labels: {}'.format(np.sum(mean_x == 0.5)/2)
+        if verbose:
+            print 'iteration {}: energy = {}'.format(iteration, energy)
 
-    if relaxed:
-        return (mean_x,)
-    else:
-        if return_energy_history:
-            return get_labelling(mean_x).astype(np.int32), energy_history
-        else:
-            return get_labelling(mean_x).astype(np.int32)
+        if iteration and np.abs(energy - energy_history[-2]) < tol:
+            if verbose:
+                print 'Converged'
+            break
+
+#        if energy > best_dual:
+#            best_dual = energy
+#        if iteration:
+#            if smart_step:
+#                grad_norm = np.sum((mean_x * 2) ** 2)
+#                if energy > prev_energy:
+#                    delta *= rho0
+#                else:
+#                    delta = max(rho1 * delta, eps)
+#                alpha = (best_dual + delta) - energy
+#                alpha /= grad_norm
+#                alpha *= gamma
+#            else:
+#                alpha = 2 / np.sqrt(iteration)
+
+    return lambda_sum, energy_history
 
