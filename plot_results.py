9ee3b3b0d48522e86b5b4a706c85190502051df7
Dmitry Kondrashkin <kondra2lp@gmail.com>
2014-05-07 22:50:16 +0400

    minor

--- plot_results.py
+++ plot_results.py
@@ -5,37 +5,40 @@ import os
 
 import results
 
+# -*- coding: utf-8 -*-
+
 # old legacy stuff
 
 def plot_syntetic_full_weak():
     # full + weak plotter for syntetic data
     # on different full-labeled training set sizes
-    os.chdir("results/syntetic")
-    weak_results = np.genfromtxt('weak_labeled.csv', delimiter=',')
-    full_results = np.genfromtxt('full_labeled.csv', delimiter=',')
+#    os.chdir("results/syntetic")
+    path = '/home/dmitry/Documents/Thesis/latent_ssvm/results/syntetic/'
+    weak_results = np.genfromtxt(path + 'weak_labeled.csv', delimiter=',')
+    full_results = np.genfromtxt(path + 'full_labeled.csv', delimiter=',')
 
     weak_results[0, 2] = 0.5
     full_results[0, 1] = 0.5
 
-    y = np.mean(weak_results[:17, :6], axis=0)
-    y_min = np.min(weak_results[:17, :6], axis=0)
-    y_max = np.max(weak_results[:17, :6], axis=0)
+    y = np.mean(weak_results[:17, 1:6], axis=0)
+    y_min = np.min(weak_results[:17, 1:6], axis=0)
+    y_max = np.max(weak_results[:17, 1:6], axis=0)
     x = np.array([0, 2, 4, 10, 25, 100])
 
     full_y = np.mean(full_results[:17, :5], axis=0)
     full_y_min = np.min(full_results[:17, :5], axis=0)
     full_y_max = np.max(full_results[:17, :5], axis=0)
 
-    pl.errorbar([0, 1, 2, 3, 4, 5], 1 - y, yerr=[y_max - y, y - y_min],
+    pl.errorbar([1, 2, 3, 4, 5], 1 - y, yerr=[y_max - y, y - y_min],
                 label='+weak')
     pl.errorbar([1, 2, 3, 4, 5], 1 - full_y,
                 yerr=[full_y_max - full_y, full_y - full_y_min], label='full')
     pl.xticks(np.arange(0, 6), x)
-    pl.title('Syntetic')
-    pl.xlabel('number of fully-labeled objects')
-    pl.ylabel('hamming loss')
+#    pl.title('Syntetic')
+    pl.xlabel(u'number of fully-labeled objects')
+    pl.ylabel('score')
     pl.ylim([0, 1])
-    pl.xlim([-0.1, 5.1])
+    pl.xlim([0.5, 5.5])
     pl.legend(loc='lower right')
 
 
