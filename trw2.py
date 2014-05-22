c7ce0cc1ea6431ab53b4144d59cfb0f54a5f526d
Dmitry Kondrashkin <kondra2lp@gmail.com>
2014-05-22 21:24:27 +0400

    iterate until convergence by lambda

--- trw2.py
+++ trw2.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 from graph_utils import decompose_graph, decompose_grid_graph
-from trw_utils import optimize_chain, optimize_kappa
+from trw_utils import *
 
 
 def trw(node_weights, edges, edge_weights, y,
         max_iter=100, verbose=0, tol=1e-3,
-        relaxed=False):
+        update_mu=50, get_energy=None):
 
     result = decompose_grid_graph([(node_weights, edges, edge_weights)])
     contains_node, chains, edge_index = result[0][0], result[1][0], result[2][0]
@@ -30,75 +30,71 @@ def trw(node_weights, edges, edge_weights, y,
 
     learning_rate = 0.1
     energy_history = []
+    primal_history = []
 
     for iteration in xrange(max_iter):
-        E = 0
         dmu = np.zeros((n_nodes, n_states))
         unaries = (node_weights - mu) * multiplier
 
-        for i, chain in enumerate(chains):
-            y_hat[i], energy = optimize_chain(chain,
-                                              lambdas[i] + unaries[chain,:],
-                                              edge_weights,
-                                              edge_index)
+        inner_energy = []
+        for inner in xrange(update_mu):
+            E = 0
+            for i, chain in enumerate(chains):
+                y_hat[i], energy = optimize_chain(chain,
+                                                  lambdas[i] + unaries[chain,:],
+                                                  edge_weights,
+                                                  edge_index)
 
-            dmu[chains[i], y_hat[i]] -= multiplier[chains[i]].flatten()
-            E += energy
+                E += energy
+
+            inner_energy.append(E)
+
+            lambda_sum = np.zeros((n_nodes, n_states), dtype=np.float64)
+            for p in xrange(n_nodes):
+                for i in contains_node[p]:
+                    pos = np.where(chains[i] == p)[0][0]
+                    lambda_sum[p, y_hat[i][pos]] += multiplier[p]
+
+            for i in xrange(len(chains)):
+                N = lambdas[i].shape[0]
+
+                lambdas[i][np.ogrid[:N], y_hat[i]] -= learning_rate
+                lambdas[i] += learning_rate * lambda_sum[chains[i],:]
+
+            if inner > 0 and np.abs(inner_energy[-2] - E) < 1e-2:
+                break
+
+        E = inner_energy[-1]
 
         y_hat_kappa, energy = optimize_kappa(y, mu, 1, n_nodes, n_states)
         E += energy
 
+        for i in xrange(len(chains)):
+            dmu[chains[i], y_hat[i]] -= multiplier[chains[i]].flatten()
         dmu[np.ogrid[:dmu.shape[0]], y_hat_kappa] += 1
 
         mu -= learning_rate * dmu
 
-        lambda_sum = np.zeros((n_nodes, n_states), dtype=np.float64)
-        for p in xrange(n_nodes):
-            assert len(contains_node[p]) == 2
-            for i in contains_node[p]:
-                pos = np.where(chains[i] == p)[0][0]
-                lambda_sum[p, y_hat[i][pos]] += multiplier[p]
-
-        for i in xrange(len(chains)):
-            N = lambdas[i].shape[0]
-
-            lambdas[i][np.ogrid[:N], y_hat[i]] -= learning_rate
-            lambdas[i] += learning_rate * lambda_sum[chains[i],:]
-
         energy_history.append(E)
+
+        if get_energy is not None:
+            primal = get_energy(get_labelling(lambda_sum))
+            primal_history.append(primal)
 
         if iteration:
             learning_rate = 1. / np.sqrt(iteration)
 
         if verbose:
-            print 'Iteration {}: energy {}'.format(iteration, E)
+            print 'Iteration {}: inner={} energy={}'.format(iteration, inner, E)
 
-        if iteration > 300 and np.abs(E - energy_history[-2]) < tol:
+        if iteration > 0 and np.abs(E - energy_history[-2]) < tol:
             if verbose:
                 print 'Converged'
             break
 
-    if relaxed:
-        unaries = np.zeros((n_nodes, n_states), dtype=np.float64)
-        for p in xrange(n_nodes):
-            mult = 1.0 / (len(contains_node[p]) + 1)
-            for i in contains_node[p]:
-                pos = np.where(chains[i] == p)[0][0]
-                unaries[p, y_hat[i][pos]] += mult
-            unaries[p, y_hat_kappa[p]] += mult
-        return get_relaxed(unaries, edges), energy_history, iteration
-    else:
-        return lambda_sum, y_hat_kappa, energy_history, iteration
+    info = {'primal': primal_history,
+            'dual': energy_history,
+            'iteration': iteration}
 
-
-def get_relaxed(unaries, edges):
-    n_edges = edges.shape[0]
-    n_states = unaries.shape[1]
-
-    pairwise = np.zeros((n_edges, n_states ** 2))
-    for i in xrange(n_edges):
-        (e1, e2) = edges[i]
-        pairwise[i,:] = np.kron(unaries[e1,:], unaries[e2,:])
-
-    return (unaries, pairwise)
+    return lambda_sum, y_hat_kappa, info
 
